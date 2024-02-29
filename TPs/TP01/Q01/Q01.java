ce3718c3c836e51c826ba83a4dba8e4144eef65f
Your Name <you@example.com>
2024-02-28 21:22:52 -0300

    100%

---
+++ TPs/TP01/Q01/Q01.java
@@ -0,0 +1,27 @@
+package TPs.TP01.Q01;
+
+import java.util.Scanner;
+
+public class Q01 {
+
+    public static boolean isPalindromo (String imput){
+        StringBuilder frase = new StringBuilder(imput);
+        return imput.equals(frase.reverse().toString());
+    } 
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String frase;
+        while (!(frase = sc.nextLine()).equals("FIM")) {
+            if(isPalindromo(frase)) {
+                System.out.println("SIM");
+            }else{
+                System.out.println("NAO");
+            }
+
+        }
+
+        sc.close();
+    }
+}
