f7c56a55541f0a04040dfbe4ddcc7aca807be386
Your Name <you@example.com>
2024-03-19 19:48:16 -0300

    100%

---
+++ TPs/TP01/Warmup01/Warmup01.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+
+public class Warmup01 {
+
+    public static int teste(String[] pokemons, int tam) {
+
+        int cont = 1;
+
+        for (int i = 1; i < tam; i++) {
+            if (!(pokemons[i].equals(pokemons[i - 1]))) {
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    public static void ordenar(String[] pokemons, int tam) {
+
+        int menor;
+        String aux;
+
+        for (int i = 0; i < tam; i++) {
+
+            menor = i;
+
+            for (int j = 1; j < tam; j++) {
+
+                if (pokemons[menor].compareTo(pokemons[j]) > 0) {
+
+                    aux = pokemons[menor];
+                    pokemons[menor] = pokemons[j];
+                    pokemons[j] = aux;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int tam = sc.nextInt();
+
+        String[] pokemons = new String[tam];
+
+        for (int i = 0; i < tam; i++) {
+            pokemons[i] = sc.next();
+        }
+
+        ordenar(pokemons, tam);
+
+        
+
+        int falta = 151 - teste(pokemons, tam);
+
+        System.out.println("Falta(m) " + falta + " pomekon(s).");
+
+        sc.close();
+
+    }
+}
